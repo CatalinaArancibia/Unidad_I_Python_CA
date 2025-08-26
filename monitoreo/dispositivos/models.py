f12f1b13da5826fd3c2fd4b5d827df58a8c3d5db
CatalinaArancibia <arancibia.ca01@gmail.com>
2025-08-26 17:47:28 -0400

    Creacios de modelos dispositivos y categorias

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Dispositivos(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+    estado = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
