14225fe5190b5fe1fa3c3a622812f732bfa29a4f
CatalinaArancibia <arancibia.ca01@gmail.com>
2025-08-29 11:36:24 -0400

    admin.py

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Zona, Dispositivo, Medicion, Alerta
+
+admin.site.register([Categoria, Zona, Dispositivo, Medicion, Alerta])
